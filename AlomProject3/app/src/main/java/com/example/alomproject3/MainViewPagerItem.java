1e1fb3d081b82b1d2bd6d001d11048d4a5b7ed47
YeseoLee <lys7442@naver.com>
2021-07-20 16:23:22 +0900

    merge to one project

---
+++ AlomProject3/app/src/main/java/com/example/alomproject3/MainViewPagerItem.java
@@ -0,0 +1,26 @@
+package com.example.alomproject3;
+
+public class MainViewPagerItem {
+
+    private  int image;
+    private String title;
+
+    public MainViewPagerItem(int image, String title)
+    {
+        this.image = image;
+        this.title = title;
+    }
+
+    public int getImage() {
+        return image;
+    }
+    public void setImage(int image) {
+        this.image = image;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
