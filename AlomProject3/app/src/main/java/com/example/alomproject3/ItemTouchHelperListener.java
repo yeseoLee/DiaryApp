c2969b067b42d6a9f58495eabd7ab1801e0b036d
YeseoLee <lys7442@naver.com>
2021-08-04 17:51:45 +0900

    .

--- AlomProject3/app/src/main/java/com/example/alomproject3/ItemTouchHelperListener.java
+++
@@ -1,7 +0,0 @@
-package com.example.alomproject3;
-
-public interface ItemTouchHelperListener {
-    boolean onItemMove(int from_position, int to_position);
-    void onItemSwipe(int position);
-
-}
